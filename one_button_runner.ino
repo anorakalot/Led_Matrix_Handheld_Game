fab5dd711150483d8707369ed2762b932377612d
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2017-12-22 16:16:17 -0800

    Add files via upload

--- one_button_runner.ino
+++ one_button_runner.ino
@@ -10,7 +10,7 @@ int enemy_2_y = 0;
 int enemy_2_x = 12;
 int jump_button = 9;
 int counter = 1;
-
+int game_speed = 90;
 void setup() {
   lc.shutdown(0,false);
   //brightness at medium
@@ -39,38 +39,55 @@ void loop() {
   player_pos_y = 0;
   lc.setLed(0,player_pos_x,player_pos_y,true);
   }
+  
   //ENEMY MOVEMENT
-  if (counter % 90 == 0){
+  if (counter % game_speed == 0){
     lc.setLed(0,enemy_1_x,enemy_1_y,false);
     enemy_1_x -= 1;
-    if (enemy_1_x < 0){
+    if (enemy_1_x == -1){
+      enemy_1_y = rand()%2;
       enemy_1_x = 10+rand()%(20-10+1);
     }
     lc.setLed(0,enemy_1_x,enemy_1_y,true);
   }
-
-  if (counter % 90 == 0){
+/*
+  if (counter % 70 == 0){
     lc.setLed(0,enemy_2_x,enemy_2_y,false);
     enemy_2_x -= 1;
-    if (enemy_2_x < 0){
-      enemy_1_x = 10 + rand()% (22-10+1);
+    if (enemy_2_x == -1){
+      enemy_2_x =  + rand()% (22-15+1);
     }
     lc.setLed(0,enemy_2_x,enemy_2_y,true);
   }
+  */
   
   //COLLISION
   if (enemy_1_x == player_pos_x && enemy_1_y == player_pos_y){
   game_over();
   runner = false;  
   }
-  
+  /*
   if (enemy_2_x == player_pos_x && enemy_2_y == player_pos_y){
   game_over();
   runner = false;  
   }
-  //
+  */
+  
   counter ++;
-   }
+  //GAME SPEED CHANGES
+  if (counter % 4000 == 0){
+  if (game_speed > 50){
+    game_speed -=10;
+  }
+    else{
+      game_speed = 50;
+    }
+  }
+
+  //end of while loop
+  }
+  
+  //outside main game while loop
    if (digitalRead(jump_button) == HIGH){
     runner = true;
    }
